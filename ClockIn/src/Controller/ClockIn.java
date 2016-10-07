d0aea9b789f047776a3439b52c8d4181b20388fa
Brian <brian31988@gmail.com>
2016-10-07 07:43:22 -0400

    clock

--- ClockIn/src/Controller/ClockIn.java
+++ ClockIn/src/Controller/ClockIn.java
@@ -5,6 +5,7 @@
  */
 package Controller;
 
+import Model.Spreadsheet;
 import View.View;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import jdk.nashorn.internal.codegen.types.Type;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -32,31 +34,9 @@ public class ClockIn {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, InvalidFormatException {
-        Calendar calendar = new GregorianCalendar();
-        Calendar cal = new GregorianCalendar();
-        //int year = calendar.get(calendar.YEAR);
-        //System.out.println(year);
-        //View view = new View();
-        /*Workbook wb = WorkbookFactory.create(new File("C:/Users/brian.marshall/Documents/testworkbook.xlsx"));
-        FileOutputStream outputStream = new FileOutputStream("C:/Users/brian.marshall/Documents/testworkbook.xlsx");
-        Sheet sheet1 = wb.getSheetAt(0);
-        sheet1.getRow(0).getCell(0).setCellValue("okokok");
-        outputStream.
-        wb.write(outputStream);
-        wb.close();
-        outputStream.close();
-*/
-        FileInputStream fsIP = new FileInputStream(new File("C:/Users/brian.marshall/Documents/SamplePayroll.xlsx"));
-        XSSFWorkbook wb = new XSSFWorkbook(fsIP);
-        XSSFSheet worksheet = wb.getSheetAt(0);
-        Date date = worksheet.getRow(4).getCell(0).getDateCellValue();
-        calendar.setTime(date);
-        System.out.println(calendar.getTime());
+        Spreadsheet spread = new Spreadsheet();
+        System.out.println(spread.getRowOfCurrentDate());
         
-        fsIP.close();
-        //FileOutputStream output_file = new FileOutputStream(new File("C:/Users/brian.marshall/Documents/SamplePayroll.xlsx"));
-        //wb.write(output_file);
-        //output_file.close();
         
     }
     
--- ClockIn/src/Model/Spreadsheet.java
+++ ClockIn/src/Model/Spreadsheet.java
@@ -17,60 +17,55 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Spreadsheet {
 
     FileInputStream fsIP;
     XSSFWorkbook wb;
     XSSFSheet worksheet;
-    Calendar calendar;
+    Calendar calendar1;
+    Calendar calendar2;
 
     public Spreadsheet() throws FileNotFoundException, IOException {
         instantiateWorksheet();
         //calendar = new GregorianCalendar();
     }
 
-    private void clockIn() throws FileNotFoundException, IOException {
-        Calendar currentDate = new GregorianCalendar();
+    public void clockIn() throws FileNotFoundException, IOException {
         getRowOfCurrentDate();
         fsIP.close();
         outputCurrentTimeToWorksheet();
-        
     }
 
-    private void instantiateWorksheet() throws FileNotFoundException, IOException {
-        fsIP = new FileInputStream(new File("C:/Users/brian.marshall/Documents/SamplePayroll.xlsx"));
+    public void instantiateWorksheet() throws FileNotFoundException, IOException {
+        fsIP = new FileInputStream(new File("C:/Users/brian/Documents/SamplePayroll.xlsx"));
         wb = new XSSFWorkbook(fsIP);
         worksheet = wb.getSheetAt(0);
     }
 
-    private int getRowOfCurrentDate() {
-        for (Row row : sheet)
-            row.getCell(0).getDateCellValue();
-        return 0;
-    }
-
-    private void outputCurrentTimeToWorksheet() throws FileNotFoundException, IOException {
-        FileOutputStream output_file = new FileOutputStream(new File("C:/Users/brian.marshall/Documents/testworkbook.xlsx"));
-        wb.write(output_file);
-        output_file.close();
-    }
-
-    public int findRow(XSSFSheet sheet) {
-
-        for (Row row : sheet) {
-            for (Cell cell : row) {
-                {
-                    if (cell.getDateCellValue().compareTo(date) == 0) {
-                        return row.getRowNum();
-                    }
-                }
+    public int getRowOfCurrentDate() {
+        Date cellDate = new Date();
+        cellDate.setTime(12/25/22);
+        calendar2 = new GregorianCalendar();
+        for (Row row : worksheet){
+            if (CellType.values(row.getCell(0) getDateCellValue()) )
+            cellDate = row.getCell(0) getDateCellValue();
+            calendar1.setTime(cellDate);
+            if((calendar1.MONTH == calendar2.MONTH) && calendar1.DAY_OF_YEAR == calendar2.DAY_OF_YEAR){
+                return row.getRowNum();
             }
         }
         return 0;
+    }
 
+    public void outputCurrentTimeToWorksheet() throws FileNotFoundException, IOException {
+        FileOutputStream output_file = new FileOutputStream(new File("C:/Users/brian/Documents/testworkbook.xlsx"));
+        wb.write(output_file);
+        output_file.close();
     }
 }
